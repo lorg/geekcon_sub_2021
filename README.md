7d664407913b9b76ef2ea9a18a434b30577bcb38
Imri Goldberg <lorgandon@gmail.com>
2021-09-30 21:30:35 +0300

    added text to the readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # geekcon_sub_2021
+
+This is the code for the Geekcon 2021 FFPS - Fish First Person Swimmer project.
+
+It was written by Imri Goldberg, Dekel Braunstein and Tomer Nussbaum.
--- geekcon_sub/geekcon_sub.ino
+++ geekcon_sub/geekcon_sub.ino
@@ -1,5 +1,5 @@
 
-#include <Servo.h> 
+#include <Servo.h>
 
 //static int LED = 2;
 static int REMOTE_R = 2;
@@ -8,21 +8,23 @@ static int REMOTE_F = 4;
 static int SERVO_PIN = 5;
 static int MOTOR_PIN = 6;
 
-Servo direction_servo; 
+Servo direction_servo;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   pinMode(MOTOR_PIN, OUTPUT);
   direction_servo.attach(SERVO_PIN);
-  
+
   pinMode(REMOTE_R, INPUT);
   pinMode(REMOTE_L, INPUT);
   pinMode(REMOTE_F, INPUT);
-  
+
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   int right = digitalRead(REMOTE_R);
   int left = digitalRead(REMOTE_L);
   int forward = digitalRead(REMOTE_F);
@@ -31,27 +33,20 @@ void loop() {
   Serial.println(forward);
 
   digitalWrite(MOTOR_PIN, LOW);
-  if (1) {
+  if (1)
+  {
     digitalWrite(MOTOR_PIN, HIGH);
   }
 
   direction_servo.write(90);
-  if (right) {
+  if (right)
+  {
     direction_servo.write(0);
   }
-  if (left) {
+  if (left)
+  {
     direction_servo.write(180);
   }
-  
-  //analogWrite(SERVO_PIN, 255);
-//  if (value > 500) {
-//    Servo1.write(90);
-//    digitalWrite(LED, HIGH);
-//  } else {
-//    Servo1.write(0);
-//    digitalWrite(LED, LOW);
-//  }
-  delay(1);
 
-  
-}   
+  delay(1);
+}
